9d8e202e0b0a7f291e24a7d90d4301eed7411c3e
BenLooper <benlooper08@gmail.com>
2020-12-31 22:05:48 -0600

    Test to render a component via a Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Oooo I'm different now wow</h1>
+      <Router>
+          <Route path='/' component={Greeting}/>
+      </Router>
     </div>
   );
 }
---
+++ src/Greeting.js
@@ -0,0 +1,7 @@
+const Greeting = () => {
+    return (
+        <h1>你好 👽</h1>
+    )
+}
+
+export default Greeting
